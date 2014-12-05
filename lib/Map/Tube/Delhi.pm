214effb1480e8e49ac3b602babfe0c8a03980261
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2014-12-05 20:36:14 +0000

    - Upgraded to use Map::Tube v2.33 as it brings in new fix and feature. - Updated unit test to reflect the changes in Map::Tube v2.33.

--- lib/Map/Tube/Delhi.pm
+++ lib/Map/Tube/Delhi.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Delhi;
 
-$Map::Tube::Delhi::VERSION = '0.12';
+$Map::Tube::Delhi::VERSION = '0.13';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ Map::Tube::Delhi - Interface to the Delhi Metro Map.
 
 =head1 VERSION
 
-Version 0.12
+Version 0.13
 
 =cut
 
