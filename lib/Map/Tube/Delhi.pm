569094a6f21e71d5641cc83a628503943f86c51f
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-02-02 17:40:10 +0000

    - Minor cosmetic changes to the pod document of Blue and Yellow line.

--- lib/Map/Tube/Delhi.pm
+++ lib/Map/Tube/Delhi.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Delhi;
 
-$Map::Tube::Delhi::VERSION   = '0.28';
+$Map::Tube::Delhi::VERSION   = '0.29';
 $Map::Tube::Delhi::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube::Delhi - Interface to the Delhi Metro Map.
 
 =head1 VERSION
 
-Version 0.28
+Version 0.29
 
 =cut
 
