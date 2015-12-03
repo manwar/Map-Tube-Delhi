73cbad04de039120ef8d8fe83dcced6b56ce08bd
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-12-03 16:12:06 +0000

    - Upgraded to use Dist::Zilla, my first attempt. - Used plugin Dist::Zilla::Plugin::Test::Map::Tube to add unit test script xt/map.t.

--- lib/Map/Tube/Delhi/Line/Violet.pm
+++ lib/Map/Tube/Delhi/Line/Violet.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Delhi::Line::Violet;
 
-$Map::Tube::Delhi::Line::Violet::VERSION   = '0.36';
+$Map::Tube::Delhi::Line::Violet::VERSION   = '0.37';
 $Map::Tube::Delhi::Line::Violet::AUTHORITY = 'cpan:MANWAR';
 
 use 5.006;
@@ -12,7 +12,7 @@ Map::Tube::Delhi::Line::Violet - Delhi Metro Map: Violet Line.
 
 =head1 VERSION
 
-Version 0.36
+Version 0.37
 
 =head1 DESCRIPTION
 
